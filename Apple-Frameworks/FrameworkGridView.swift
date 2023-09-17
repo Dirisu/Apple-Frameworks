c7131b0d7909028002a8b906fbc191754e5134a8
Dirisu <dirisumarvellous94@gmail.com>
2023-09-17 16:58:57 +0100

    set up the detail grid view

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -8,13 +8,70 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())
+    ]
+    
     var body: some View {
-        FrameWorkTitleView(name: "App Clips", imageName: "app-clip")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    
+                    ForEach(MockData.frameworks) {framework in
+                        FrameWorkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle(" Frameworks")
+        }
+
     }
 }
 
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct FrameWorkTitleView: View {
+    
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
+}
+
+struct FrameWorkDetailView: View {
+    
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+            Text(framework.description)
+        }
+        .padding()
     }
 }
