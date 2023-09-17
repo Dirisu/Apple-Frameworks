49a1c1f4bdb7f28fecb64680fd40626bba9f48cd
Dirisu <dirisumarvellous94@gmail.com>
2023-09-17 14:05:20 +0100

    set up project, assets and icons

---
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Marvellous Dirisu on 17/09/2023.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        FrameWorkTitleView(name: "App Clips", imageName: "app-clip")
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
